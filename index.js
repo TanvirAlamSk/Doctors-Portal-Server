9b265a40ca8d2d20adde1923556f43a2e4db74f0
Tanvir Alam <ta89725@gmail.com>
2023-09-29 10:05:22 +0600

    deploy

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ var jwt = require('jsonwebtoken');
 const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
 
 app.use(cors());
+// app.use(express.static("public"))
 app.use(express.json())
+
+
 
 app.get("/", (req, res) => {
     res.send("Server is Running");
@@ -46,11 +50,53 @@ async function run() {
         const bookingCollection = client.db("doctors-portal").collection("bookingCollection");
         const userCollection = client.db("doctors-portal").collection("users");
         const doctorsCollection = client.db("doctors-portal").collection("doctors");
+        const paymentCollection = client.db("doctors-portal").collection("payment");
+
+        app.post("/create-payment-intent", async (req, res) => {
+            const data = req.body
+            const price = data.price
+            const amount = price * 100;
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount,
+                currency: "usd",
+                "payment_method_types": [
+                    "card"
+                ],
+            });
+            res.send({
+                clientSecret: paymentIntent.client_secret,
+            });
+        })
 
         app.post("/jwt", async (req, res) => {
             const token = jwt.sign(req.body, process.env.JWT_TOKEN, { expiresIn: "10h" })
             res.send({ token })
         })
+
+        // 
+        const adminVerify = async (req, res, next) => {
+            const decodeEmail = req.decoded.email;
+            const query = { email: decodeEmail }
+            console.log(req.decoded.email)
+            const result = await userCollection.findOne(query)
+
+            if (result.role != "Admin") {
+                return res.status(403).send({ massage: "Forbidden Access" })
+            }
+            next()
+        }
+        // 
+        // app.get("/add-price", async (req, res) => {
+        //     const query = {}
+        //     const option = { upsert: true }
+        //     const updateDoc = {
+        //         $set: {
+        //             price: 99
+        //         }
+        //     }
+        //     const result = await appointmentOptions.updateMany(query, updateDoc, option);
+        //     res.send(result);
+        // })
 
         app.get("/appointmentoptions", async (req, res) => {
             const date = req.query.date
@@ -76,6 +122,7 @@ async function run() {
             const cursor = await appointmentOptions.find(query).project({ name: 1 }).toArray()
             res.send(cursor)
         })
+        //bookinh
 
         app.get("/bookings", verifyToken, async (req, res) => {
             const decoded = req.decoded;
@@ -87,10 +134,20 @@ async function run() {
             res.send(cursor)
         })
 
+        app.get("/bookings/:id", async (req, res) => {
+            const query = {
+                _id: new ObjectId(req.params.id)
+            }
+            const cursor = await bookingCollection.findOne(query)
+            res.send(cursor);
+        })
+
         app.post("/bookings", async (req, res) => {
             const result = await bookingCollection.insertOne(req.body)
             res.send(result)
         })
+
+        //user
         app.get("/users", async (req, res) => {
             const query = {}
             const cursor = await userCollection.find(query).toArray()
@@ -105,16 +162,8 @@ async function run() {
 
         })
 
-        app.put("/users/:id", verifyToken, async (req, res) => {
-            const decodedEmail = req.decoded.email
-            const fitterAdmin = {
-                email: decodedEmail
-            }
+        app.put("/users/:id", verifyToken, adminVerify, async (req, res) => {
 
-            const checkAdmin = await userCollection.findOne(fitterAdmin)
-            if (checkAdmin.role !== "Admin") {
-                return res.status(403).send({ massage: "Forbidden Access" })
-            }
             const id = req.params.id
             const query = { _id: new ObjectId(id) }
             const option = { upsert: true }
@@ -127,25 +176,41 @@ async function run() {
             res.send(result)
         })
 
-        app.post("/users", async (req, res) => {
+        app.post("/users", verifyToken, async (req, res) => {
             const result = await userCollection.insertOne(req.body)
             res.send(result)
         })
-
-        app.get("/doctors", async (req, res) => {
+        //doctor
+        app.get("/doctors", verifyToken, adminVerify, async (req, res) => {
             const query = {};
             const cursor = await doctorsCollection.find(query).toArray()
             res.send(cursor)
         })
-        app.post("/doctors", async (req, res) => {
+        app.post("/doctors", verifyToken, adminVerify, async (req, res) => {
             const result = await doctorsCollection.insertOne(req.body)
             res.send(result)
         })
-        app.delete("/doctors/:id", async (req, res) => {
+        app.delete("/doctors/:id", verifyToken, adminVerify, async (req, res) => {
             const id = req.params.id
             const query = { _id: new ObjectId(id) }
             const result = await doctorsCollection.deleteOne(query)
             res.send(result);
+        })
+
+        //payment
+        app.post("/payment", async (req, res) => {
+            const result = await paymentCollection.insertOne(req.body)
+            const id = req.body.bookingId
+            const query = { _id: new ObjectId(id) }
+            const option = { upsert: true }
+            const updateDoc = {
+                $set: {
+                    paid: true,
+                    transectionId: req.body.transectionId
+                }
+            }
+            const updateResult = await bookingCollection.updateOne(query, updateDoc, option)
+            res.send(result)
         })
 
     } finally {
